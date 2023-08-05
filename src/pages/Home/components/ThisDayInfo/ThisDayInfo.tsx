088315084b06d5164b6353e3be48c9455d02dfa2
Пользователь <kuleshov000@gmail.com>
2023-08-05 15:54:54 +0300

    added ThisDayInfo component

--- src/pages/Home/components/ThisDayInfo/ThisDayInfo.tsx
+++ src/pages/Home/components/ThisDayInfo/ThisDayInfo.tsx
@@ -1,35 +1,31 @@
 import React from 'react';
 import s from './ThisDayInfo.module.scss';
 import cloud from '../../../../images/cloud.webp';
-import temp from '../../../../images/temp.svg';
-import pressure from '../../../../images/pressure.svg';
-import precipitation from '../../../../images/precipitation.svg';
-import wind from '../../../../images/wind.svg';
 import ThisDayItem from './ThisDayItem';
 
 type Props = {};
 
 const items = [
     {
-        item_icon: {temp},
+        item_icon: 'temp',
         item_name: 'Температура',
         item_value: '20° - ощущается как 17°'
     },
 
     {
-        item_icon: {pressure},
+        item_icon: 'pressure',
         item_name: 'Давление',
         item_value: '765 мм ртутного столба - нормальное'
     },
 
     {
-        item_icon: {precipitation},
+        item_icon: 'precipitation',
         item_name: 'Осадки',
         item_value: 'Без осадков'
     },
 
     {
-        item_icon: {wind},
+        item_icon: 'wind',
         item_name: 'Ветер',
         item_value: '3 м/с юго-запад - легкий ветер'
     },
@@ -40,7 +36,7 @@ function ThisDayInfo({}: Props) {
         <div className={s.info}>
             <img className={s.info__img} src={cloud} alt="cloud" />
 
-            {items.map(obj => <ThisDayItem key={obj.item_name} item_name={obj.item_name} />)}
+            {items.map(obj => <ThisDayItem key={obj.item_name} {...obj} />)}
         </div>
     );
 }
--- src/pages/Home/components/ThisDayInfo/ThisDayItem.tsx
+++ src/pages/Home/components/ThisDayInfo/ThisDayItem.tsx
@@ -1,13 +1,48 @@
 import React from 'react';
+import s from './ThisDayItem.module.scss';
+import { ReactComponent as Temp } from '../../../../images/temp.svg';
+import { ReactComponent as Pressure } from '../../../../images/pressure.svg';
+import { ReactComponent as Precipitation } from '../../../../images/precipitation.svg';
+import { ReactComponent as Wind } from '../../../../images/wind.svg';
 
 type Props = {};
 
 type ItemNameProp = {
     item_name: string;
-}
+    item_value: string;
+    item_icon: string;
+};
 
-const ThisDayItem = ({ item_name }: ItemNameProp) => {
-    return <div>{item_name}</div>;
+const renderIcon = (iconName: string) => {
+    switch (iconName) {
+        case 'temp':
+            return <Temp />;
+            break;
+        case 'pressure':
+            return <Pressure />;
+            break;
+        case 'precipitation':
+            return <Precipitation />;
+            break;
+        case 'wind':
+            return <Wind />;
+            break;
+        default:
+            return null;
+            break;
+    }
+};
+
+const ThisDayItem = ({ item_name, item_icon, item_value }: ItemNameProp) => {
+    return (
+        <div className={s.item}>
+            <div className={s.item__icon}>
+                {renderIcon(item_icon)}
+            </div>
+            <div className={s.item__name}>{item_name}</div>
+            <div className={s.item__value}>{item_value}</div>
+        </div>
+    );
 };
 
 export default ThisDayItem;
