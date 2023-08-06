56c8ab2fded9a540339fad47dbb91c4f3b13d59e
Пользователь <kuleshov000@gmail.com>
2023-08-06 14:54:28 +0300

    added Popup component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import MonthStatistics from "./pages/MonthStatistics/MonthStatistics";
-import Header from "./shared/Header/Header";
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import MonthStatistics from './pages/MonthStatistics/MonthStatistics';
+import Header from './shared/Header/Header';
+import Popup from './shared/Popup/Popup';
 
 const App = () => {
-    return <div className="container">
-        <Header />
+    return (
+        <div className='global-container'>
+            <Popup />
+            <div className="container">
+                <Header />
 
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path='/month-stat' element={<MonthStatistics />} />
-        </Routes>
-    </div>;
-}
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/month-stat" element={<MonthStatistics />} />
+                </Routes>
+            </div>
+        </div>
+    );
+};
 
 export default App;
